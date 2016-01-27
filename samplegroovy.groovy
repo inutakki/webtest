f444c562bbd024c69bc4d28e6d5aa085ccc80888
Indira Nutakki <indira.nutakki@yahoo.com>
2016-01-26 23:38:50 -0800

    another groovy file

---
+++ samplegroovy.groovy
@@ -0,0 +1,13 @@
+/**
+ * Created by indira.nutakki on 1/25/2016.
+ */
+
+def GroovyScriptObject =  new CD_ConfigLoader();
+return GroovyScriptObject.buildStep();
+
+/*
+println( obj.artifactoryURL)
+println( obj.artifactoryPublishUser)
+println( obj.artifactoryPublishPassword)
+println(obj.releaseNumber)
+println(obj.buildStep())*/
