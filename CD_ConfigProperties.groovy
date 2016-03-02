f4abf0f013032c61076a815a46fc0c79831eeddc
inutakki <inutakki@yahoo.com>
2016-03-01 22:18:47 -0800

    Update CD_ConfigProperties.groovy

--- CD_ConfigProperties.groovy
+++ CD_ConfigProperties.groovy
@@ -3,10 +3,12 @@ artifactoryPublishUser="asdf"
 artifactoryPublishPassword="sdj"
 settingsFilePath="jskfjslafk.xml"
 releaseNumber="2016.02.0"
-def String[] s=["ci", "release"] as String[];
+def strArray = new String[2]
+strArray[0]="ci"
+strArray[1]="release"
 
 def getArr(){
-  return s[];
+  return strArray;
 }
 
 
